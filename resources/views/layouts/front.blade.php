e93e5b6f21a79ce20b55204f0c6700cc5445120a
haritsf <haritsf@hotmail.com>
2020-05-02 15:44:26 +0700

    Footer only Landing

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -45,53 +45,6 @@
 
   @yield('content')
 
-  <!-- Footer Section -->
-  <footer class="main-footer py-3 bg-dark" @if (Route::is('landing'))  @else style="width: 100%; position: fixed; left: 0; bottom: 0;" @endif>
-    <div class="container pl-0 pr-0">
-      <div class="col-lg-12 col-md-12 col-sm-12 ml-auto mr-auto text-left">
-        <h4 class="text-light mt-3"><b>SIBUMDES</b></h4>
-        <div class="row">
-          <div class="col-lg-4 col-md-6 col-sm-12">
-            <p class="text-secondary text-justify">SIBUMDEs adalah Sistem Informasi Badan Usaha Milik Desa
-              yang berisi
-              data dan informasi tentang BUMDes, serta sebagai <i>market place</i> BUMDesa yang ada di
-              Provinsi
-              Banten.<br>
-              Jika Anda ingin bantuan atau memiliki masukan, silahkan hubungi kami</p>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-12">
-            <h6 class="text-secondary mb-0"><b>Bantuan</b></h6>
-            <ul class="list-unstyled">
-              <li><a href="{{asset('files/cara-penggunaan.pdf')}}" target="_blank" class="text-secondary">Cara Mendaftar</a></li>
-              <li><a href="#" class="text-secondary">Cara Mengupdate Data</a></li>
-            </ul>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-12">
-            <h6 class="text-secondary mb-0"><b>Dinas Pemberdayaan Masyarakat dan Desa Provinsi Banten</b></h6>
-            <table class="text-secondary">
-              <tr>
-                <td>Email</td>
-                <td>: dpmd@bantenprov.go.id</td>
-              </tr>
-            </table><br />
-            <h6 class="text-secondary mb-0"><b>Forum BUMDEs Indonesia Provinsi Banten</b></h6>
-            <table class="text-secondary">
-              <tr>
-                <td>Kontak</td>
-                <td>: +6281906000006</td>
-              </tr>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-12 col-md-12 col-sm-12 text-center">
-      <p class="text-center pb-4 my-0">&copy; Copyright — SIBUMDes DPMD Provinsi Banten. All Rights Reserved 2020
-      </p>
-    </div>
-  </footer>
-  <!-- / Footer Section -->
-
 </body>
 
 </html>
